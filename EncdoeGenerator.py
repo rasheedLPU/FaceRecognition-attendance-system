8bf4b47def3001a335419f790e863bfc8a91d18c
rasheed1026 <130817368+rasheedLPU@users.noreply.github.com>
2024-05-05 11:50:53 +0530

    Add files via upload

---
+++ EncdoeGenerator.py
@@ -0,0 +1,32 @@
+import cv2
+import face_recognition
+import os
+
+# Importing the student images
+folderPath = 'Images'
+PathList = os.listdir(folderPath)
+print(PathList)
+imgList = []
+studentIds = []
+for path in PathList:
+    imgList.append(cv2.imread(os.path.join(folderPath, path)))
+    studentIds.append(os.path.splitext(path)[0])
+    #print(path)
+    #print(os.path.splitext(path)[0])
+print(studentIds)
+
+
+
+
+def findEncodings(imagesList):
+    encodeList = []
+    for img in imagesList:
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+print("Encoding started...")
+enccodeListKnown = findEncodings(imgList)
+print("Encoding completed")
+
